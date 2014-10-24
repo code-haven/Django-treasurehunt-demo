a3ec9f4deeff808a052acdb4b95b5c14ea7a2c06
Abhinav I <abhinav147@rocketmail.com>
2014-10-24 09:21:09 +0530

    Fixed media

--- treasurehunt/urls.py
+++ treasurehunt/urls.py
@@ -11,6 +11,6 @@ urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'treasurehunt.views.index'),
     url(r'', include('registration.backends.default.urls'))
-) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
